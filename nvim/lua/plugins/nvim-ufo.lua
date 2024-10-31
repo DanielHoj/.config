805b5c9b3b9b9628f9d2df72c324b9fd443cb9cf
YDNH <ydnh@novonordisk.com>
2024-10-31 10:42:24 +0100

    Add ufo

---
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,18 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = { 'kevinhwang91/promise-async' },
+  opts = {
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('local_detach_ufo', { clear = true }),
+      pattern = opts.filetype_exclude,
+      callback = function()
+        require('ufo').detach()
+      end,
+    })
+
+    vim.opt.foldlevelstart = 99
+    require('ufo').setup(opts)
+  end,
+}
